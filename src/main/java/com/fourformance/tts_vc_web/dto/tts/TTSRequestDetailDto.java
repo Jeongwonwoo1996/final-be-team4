464b2398be5528798e7be262cdd9bde5228bf142
jeongwonwoo <zkjjx@naver.com>
2024-11-28 15:33:02 +0900

    개별행으로 상태처리할 수 있도록 수정 및 DTO 분리

--- src/main/java/com/fourformance/tts_vc_web/dto/tts/TTSRequestDetailDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/tts/TTSRequestDetailDto.java
@@ -1,6 +1,5 @@
 package com.fourformance.tts_vc_web.dto.tts;
 
-import com.fourformance.tts_vc_web.common.constant.APIUnitStatusConst;
 import lombok.*;
 
 @Getter
@@ -8,7 +7,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class TTSResponseDetailDto {
+public class TTSRequestDetailDto {
 
     private Long id; // 상세 정보 ID
     private Long projectId; // 프로젝트 ID
@@ -19,7 +18,4 @@ public class TTSResponseDetailDto {
     private Boolean isDeleted; // 삭제 여부
     private Integer unitSequence; // 단위 시퀀스
     private Long UnitVoiceStyleId; // 스타일 이름 (optional, lazy load 대신 포함할 수 있는 필드)
-    private String fileUrl; // aws s3 업로드 경로
-//    private String errorMessage; // 실패 시 오류 메시지
-    private APIUnitStatusConst apiUnitStatus; // SUCCESS 또는 FAILURE
 }
