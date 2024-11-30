9cbbbc78da311e383c7467da50a7f64d730b6fcc
HiSeungmin <osminosmin7180@gmail.com>
2024-11-30 19:20:53 +0900

    ìChore: ìCunsumer ì수정중

--- src/main/java/com/fourformance/tts_vc_web/service/common/TaskConsumer.java
+++ src/main/java/com/fourformance/tts_vc_web/service/common/TaskConsumer.java
@@ -1,15 +1,31 @@
 package com.fourformance.tts_vc_web.service.common;
 
 import com.fourformance.tts_vc_web.common.config.TaskConfig;
+import com.fourformance.tts_vc_web.common.constant.TaskStatusConst;
+import com.fourformance.tts_vc_web.domain.entity.Task;
+import com.fourformance.tts_vc_web.repository.TaskHistoryRepository;
+import com.fourformance.tts_vc_web.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@RequiredArgsConstructor
 public class TaskConsumer {
+
+    private final TaskRepository taskRepository;
+    private final TaskHistoryRepository taskHistoryRepository;
 
     @RabbitListener(queues = TaskConfig.TTS_QUEUE)
     public void handleTTSTask(String message) {
         System.out.println("TTS audio task : " + message);
+
+        // 상태 업데이트
+        // Task task = 받아온거;
+        // task.updateStatus(TaskStatusConst.RUNNABLE);
+
 
         // TTS 생성 로직 구현
         try{
