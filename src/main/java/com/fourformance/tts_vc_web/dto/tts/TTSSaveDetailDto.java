0ba56b00f519ee5cf10e6a95ba461d5cd0bee959
yr0202 <yooram0202@naver.com>
2024-12-04 12:54:31 +0900

    Fix: tts/load 수정

--- src/main/java/com/fourformance/tts_vc_web/dto/tts/TTSSaveDetailDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/tts/TTSSaveDetailDto.java
@@ -19,5 +19,5 @@ public class TTSSaveDetailDto {
     private Float unitVolume; // 단위 볼륨
     private Boolean isDeleted; // 삭제 여부
     private Integer unitSequence; // 단위 시퀀스
-    private Long UnitVoiceStyleId; // 스타일 이름 (optional, lazy load 대신 포함할 수 있는 필드)
+    private Long unitVoiceStyleId; // 스타일 이름
 }
