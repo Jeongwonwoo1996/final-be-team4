ea16cd1f03556afd037388657c2c0737f4490ef1
Luti <wns6619@gmail.com>
2024-12-04 13:34:45 +0900

    test commit

--- src/main/java/com/fourformance/tts_vc_web/controller/concat/ConcatViewController_team_aws.java
+++ src/main/java/com/fourformance/tts_vc_web/controller/concat/ConcatViewController_team_aws.java
@@ -53,7 +53,7 @@ public class ConcatViewController_team_aws {
 
         // Member 객체 조회
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new IllegalStateException("Member not found"));
+                .orElseThrow(() -> new BusinessException(ErrorCode.MEMBER_NOT_FOUND));
 
         Long projectId = concatService.saveConcatProject(concatSaveDto, files, member);
 
@@ -92,15 +92,13 @@ public class ConcatViewController_team_aws {
         if (deleteDto.getDetailIds() != null) {
             projectService.deleteSelectedDetails(deleteDto.getDetailIds());
         }
-
-        // 선택된 오디오 삭제
         if (deleteDto.getAudioIds() != null) {
             projectService.deleteAudioIds(deleteDto.getAudioIds());
-        }
 
-        // 버킷에서 오디오 파일 삭제
-        for (Long audioId : deleteDto.getAudioIds()) {
-            s3Service.deleteAudioMember(audioId);
+            // 버킷에서 오디오 파일 삭제
+            for (Long audioId : deleteDto.getAudioIds()) {
+                s3Service.deleteAudioMember(audioId);
+            }
         }
 
         return DataResponseDto.of("", "선택된 모든 항목이 정상적으로 삭제되었습니다.");
