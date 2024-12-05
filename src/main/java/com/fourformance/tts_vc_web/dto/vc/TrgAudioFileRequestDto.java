7bd782ef78eab03f8fd6ddac071d2cb2acb77247
Jayhong0731 <woghd5155@gmail.com>
2024-12-06 01:58:22 +0900

    Fix: Dto 수정

--- src/main/java/com/fourformance/tts_vc_web/dto/vc/TrgAudioFileRequestDto.java
+++ src/main/java/com/fourformance/tts_vc_web/dto/vc/TrgAudioFileRequestDto.java
@@ -1,5 +1,6 @@
 package com.fourformance.tts_vc_web.dto.vc;
 
+import com.fourformance.tts_vc_web.common.constant.AudioType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,5 @@ import lombok.Setter;
 @NoArgsConstructor
 public class TrgAudioFileRequestDto extends AudioFileDto {
     private Long s3MemberAudioMetaId; //  S3 메타 정보 (DB에서 조회)
+    private AudioType audioType; // 오디오 타입 (VC_TRG 또는 VC_SRC)
 }
