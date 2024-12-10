97888827b6f5ba8ea5cbe5869f1f4c33c6d9e52c
yr0202 <yooram0202@naver.com>
2024-12-11 00:14:31 +0900

    Fix: jackson 설정 수정

--- src/main/java/com/fourformance/tts_vc_web/common/config/JacksonConfig.java
+++ src/main/java/com/fourformance/tts_vc_web/common/config/JacksonConfig.java
@@ -2,6 +2,8 @@ package com.fourformance.tts_vc_web.common.config;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,9 @@ public class JacksonConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         // 알 수 없는 속성을 무시
+
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return objectMapper;
     }
